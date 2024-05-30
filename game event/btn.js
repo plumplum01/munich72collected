132ec5e75ff8d30bc046d398f34333a6823699ec
plumplum01 <plumplum010101@gmail.com>
2024-05-31 04:14:08 +0900

    btn align

--- game event/btn.js
+++ game event/btn.js
@@ -2,31 +2,31 @@
 const selectors = document.querySelectorAll(".selector");
 
 selectors.forEach(selector => {
-  const knob = selector.querySelector(".knob");
-  const ul = selector.querySelector("ul");
-  const radios = selector.querySelectorAll('input[type="radio"]');
+const knob = selector.querySelector(".knob");
+const ul = selector.querySelector("ul");
+const radios = selector.querySelectorAll('input[type="radio"]');
 
-  function handleClick() {
+function handleClick() {
     selector.classList.toggle("active");
 
     if (selector.classList.contains("active")) {
-      selector.style.removeProperty("--angle");
+        selector.style.removeProperty("--angle");
     } else {
-      selector.style.setProperty("--angle", "-90deg");
+        selector.style.setProperty("--angle", "-90deg");
 
       // Check the first one on close
-      radios.forEach((radio, index) => {
+    radios.forEach((radio, index) => {
         if (index !== 0) {
-          radio.checked = false;
+            radio.checked = false;
         } else {
-          radio.checked = true;
+            radio.checked = true;
         }
-      });
+    });
     }
-  }
+}
 
 
-  knob.addEventListener("click", handleClick);
+    knob.addEventListener("click", handleClick);
 });
 //# sourceURL=pen.js
     
